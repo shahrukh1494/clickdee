0c91a109aa4f3c0933ba5965069b3a2693715744
Shahrukh <shahrukh1494@gmail.com>
2023-07-24 16:44:08 +0530

    added cursor pointer and default

--- src/ChooseUs.js
+++ src/ChooseUs.js
@@ -3,11 +3,11 @@ import React from "react";
 const ChooseUs = () => {
   return (
     <div className="relative py-12 px-[100px] mt-12 grid grid-rows-2 grid-cols-3 gap-8">
-      <div className="relative block row-span-1">
+      <div className="cursor-default relative block">
         <div className="text-blue-900 font-medium leading-[60px] text-5xl">
           Why The Industry Chooses Clickdee?
         </div>
-        <div className="pt-8 col-span-1">
+        <div className="pt-8">
           We understand performance marketing from every angle and every stage
           of the funnel. Our clients trust that we know what metrics move their
           business towards growth. Our pulishers and affiliate partners know
@@ -15,23 +15,23 @@ const ChooseUs = () => {
           partnerships.
         </div>
       </div>
-      <div className="p-12 text-blue-900 text-3xl text-center">
+      <div className="cursor-pointer p-12 text-blue-900 text-3xl text-center">
         <div className="px-12 text-left">Choose Your Local Targeting</div>
       </div>
-      <div className="p-12 text-blue-900 text-3xl text-center">
+      <div className="cursor-pointer p-12 text-blue-900 text-3xl text-center">
         <div className="px-12 text-left">Track Your Conversion</div>
       </div>
       <div className="grid grid-cols-2">
         <div></div>
-        <div className="p-12 text-blue-900 text-3xl text-center">
+        <div className="cursor-pointer p-12 text-blue-900 text-3xl text-center">
           <div className="px-12 text-left">Customized Campaigns</div>
         </div>
       </div>
 
-      <div className="p-12 text-blue-900 text-3xl text-center">
+      <div className="cursor-pointer p-12 text-blue-900 text-3xl text-center">
         <div className="px-12 text-left">Get Dedicated Support Team</div>
       </div>
-      <div className="p-12 text-blue-900 text-3xl text-center">
+      <div className="cursor-pointer p-12 text-blue-900 text-3xl text-center">
         <div className="px-12 text-left">Quality Assurance</div>
       </div>
     </div>
